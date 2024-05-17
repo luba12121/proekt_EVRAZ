9fcd5b901276c232f06628eb9b1207ef1034064c
luba12121 <kadnikova.lubasha@yandex.ru>
2024-05-17 15:02:20 +0200

    ничего больше чем ничего

--- lubasha4oktober23/Бот администратор.py
+++ lubasha4oktober23/Бот администратор.py
@@ -5,23 +5,38 @@ from telebot import types
 token2 = '6909925991:AAEi24htnDOZeUWEoZxQOXHTh_9RHEz8fk8'
 bot = telebot.TeleBot(token2)
 
+dannie_fio = []
+
+def sozdanie_soobshenia(message):
+    message = []
+
+
+
 def get_employee(message):
+    global dannie_fio
     print(message.text)
-    dannie_fio = []
-    dannie_fio.append(message)
+    dannie_fio.append(message.text)
     bot.send_message(message.chat.id, text='Данные записаны', parse_mode='Markdown')
     textpol2 = 'Укажите телеграмм сотрудника'
     bot.send_message(message.chat.id, text=textpol2, parse_mode='Markdown')
     bot.register_next_step_handler(message, get_save)
+
 def get_save(message):
+    global dannie_fio
+    # global message
     file = open('dannie_sotrudnika.json', 'w+', encoding='utf-8')
-    #  dannie_sotrudnika= {
-    #     "name": ,
-    #     "tg":
-    #
-    # }
-    # json.dump(dannie_sotrudnika, file, ensure_ascii=False)
-    # file.close()
+    dannie_sotrudnika= {
+        "name": dannie_fio[0],
+        "tg": message.text
+    }
+    json.dump(dannie_sotrudnika, file, ensure_ascii=False)
+    file.close()
+    bot.send_message(message.chat.id, text='Данные записаны', parse_mode='Markdown')
+    tekst = 'Хотите добавить еще одного сотрудника?'
+    bot.send_message(message.chat.id, text=tekst, parse_mode='Markdown')
+    # bot.register_next_step_handler(message, sozdanie_soobshenia)
+    if tekst == 'да':
+
 
 
 @bot.message_handler(content_types=['text'])
@@ -49,12 +64,30 @@ def get_message(message):
 def callback_worker(call):
 
     if call.data == 'Посмотреть сотрудников':
-        bot.send_message(call.from_user.id, text=call.message, parse_mode='Markdown')
-
+        file1 = open('dannie_sotrudnika.json', 'r+', encoding='utf-8')
+        workers = json.load(file1)
+        print(workers)
+        bot.send_message(call.from_user.id, text = workers['name'] , parse_mode='Markdown')
+        bot.send_message(call.from_user.id, text= workers['tg'], parse_mode='Markdown')
     elif call.data == 'Добавить сотрудника':
-        textpol = ('Укажите ФИО сотрудника')
-        bot.send_message(call.from_user.id, text=textpol, parse_mode='Markdown')
-        bot.register_next_step_handler(call.message, get_employee)
+            textpol = ('Укажите ФИО сотрудника')
+            bot.send_message(call.from_user.id, text=textpol, parse_mode='Markdown')
+            bot.register_next_step_handler(call.message, get_employee)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#что бы добавление была бесконечным, и что бы выводилось хотя бы
 
 
 bot.polling(none_stop=True)
